ca7f8d130e7565f3870181d67b1684e74f28b27a
gortega2 <fpsgus97@gmail.com>
2021-09-21 21:07:38 -0600

    Added error code for verb commands, basic GET implemented

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 #  coding: utf-8 
 import socketserver
+from urllib import parse
+import os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -26,13 +28,77 @@ import socketserver
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
-
+#HEADER = 64
+FORMAT = 'utf-8'
 class MyWebServer(socketserver.BaseRequestHandler):
     
+
+    def parse_request(self, msg):
+        split_msg = msg.split("\r\n")
+        print(split_msg)
+        if 'GET' in split_msg[0]:
+            url = split_msg[0].split()[1]
+            print("Printing URL")
+            print(url)
+            print(parse.urlparse(url))
+            test = self.do_GET(url)
+            self.send_message(200, msg=test)
+            return
+        else:
+            print("send error code")
+            self.do_GET(405)
+
+
+
+        return
+
+    def send_message(self, code, msg=''):
+        if code == 405:
+            data = "HTTP/1.1 405 Method Not Allowed\r\n"
+            data += "Connection: close\r\n"
+            self.request.sendall(bytearray(data, FORMAT))
+        elif code == 200:
+            data = "HTTP/1.1 200 OK\r\n"
+            data += 'Content-Type: text/html\r\n'
+            data += 'Connection: close \r\n\r\n'
+            data += str(msg)
+            
+            self.request.sendall(bytearray(data, FORMAT))
+
+
+        return
+
+    def do_GET(self, request):
+        if request == '/':
+
+            #with open('www/base.css', 'r') as f:
+                #data = f.read()
+
+            #TODO: FIX THE PATH AS IT WILL NOT WORK ON LAB MACHINES, ONLY ON THIS MACHINE!!!!!!!!!!!!
+            with open('www/index.html' ,'r') as f:
+                data = f.read()
+        
+            print(data)
+            return data
+
+        return
+
+
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        '''
+        data = "HTTP/1.1 200 OK\r\n"
+        data += "TESTING \r\n"
+        data += "Connection: close \r\n\r\n"
+        '''
+        #self.request.sendall(bytearray("HTTP/1.1 200 OK",'utf-8'))
+        #self.request.sendall(bytearray("TESTING", FORMAT))
+        #self.request.sendall(bytearray(data, FORMAT))
+        print("UTF decode: ",self.data.decode(FORMAT))
+        #print("Attempting to print path")
+        #print(parse.urlparse(self.data.decode(FORMAT)).port)
+        self.parse_request(self.data.decode(FORMAT))
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
@@ -44,3 +110,4 @@ if __name__ == "__main__":
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
     server.serve_forever()
+    server.shutdown()
