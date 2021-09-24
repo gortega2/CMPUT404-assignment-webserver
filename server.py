868a1cb504b014be66e5c871ac2ae02146f18a89
gortega2 <fpsgus97@gmail.com>
2021-09-23 19:08:26 -0600

    All tests pass (runner.sh)

--- server.py
+++ server.py
@@ -35,122 +35,105 @@ FORMAT = 'utf-8'
 
 CWD = str(Path.cwd()) + '/www'
 mime_dict = {'.css':'text/css', '.html':'text/html'}
-http_code_dict = {200:"OK", 404:"File Not Found", 405:"Method Not Allowed"}
+http_code_dict = {200:"OK", 303:"See Other", 404:"Not Found", 405:"Method Not Allowed"}
 #print(CWD)
 class MyWebServer(socketserver.BaseRequestHandler):
     
 
     def parse_request(self, msg):
         split_msg = msg.split("\r\n")
-        print(split_msg)
         if 'GET' in split_msg[0]:
             url = split_msg[0].split()[1]
-            print("Printing URL")
-            print(url)
-            print(parse.urlparse(url))
-            print("THE MIME TYPE OF THE URL IS: {}".format(mimetypes.guess_type(url)))
             mime_type = mimetypes.guess_type(url)
-            test = self.do_GET(url)
+            code, new_url = self.test_url(url)
+            print('new url: %s' % (new_url))
+            self.send_message(code, new_url)
+        else:
+            self.send_message(405, None)
+            '''
+            print(self.test_url(url))
             if test != None:
                 self.send_message(200, mime_type,msg=test)
                 return
+            elif test == 301:
+                self.send_message(301, msg=url)
             else:
                 self.send_message(404)
 
         else:
             print("send error code")
-            self.do_GET(405)
+            self.send_message(405)
+            '''
 
 
 
         return
 
-    def send_message(self, code, type='', msg=''):
-        print(self.send_http_headers(code, type, msg))
-        self.request.sendall(bytearray(self.send_http_headers(code, type, msg), FORMAT))
-        '''
-        if code == 405:
-            data = "HTTP/1.1 405 Method Not Allowed\r\n"
-            data += "Connection: close\r\n\r\n"
-            self.request.sendall(bytearray(data, FORMAT))
-        elif code == 200:
-            data = "HTTP/1.1 200 OK\r\n"
-            data += 'Content-Type: text/html\r\n'
-            data += 'Connection: close \r\n\r\n'
-            data += str(msg)
-            
-            self.request.sendall(bytearray(data, FORMAT))
-        
-        elif code == 404:
-            data = 'HTTP/1.1 404 File Not Found'
-            data += "Connection: close \r\n\r\n"
-            self.request.sendall(bytearray(data, FORMAT))
-        '''
-
+    def send_message(self, code, url):
+        #rint(self.send_http_headers(code, type, msg))
+        #self.request.sendall(bytearray(self.send_http_headers(code, type, msg), FORMAT))
+        mess_send = self.send_http_headers(code, url)
+        test = mess_send.split('\r\n')
+        for x in test:
+            print(x)
+            if 'Connection: close' in x:
+                break
+        print('\n')
+        self.request.sendall(bytearray(mess_send, FORMAT))
         return
 
-    def send_http_headers(self, code, type, msg):
+    #Takes in the url the user is requesting and returns an http code depending on if it;s correct
+    def test_url(self, url):
+        path = CWD + url
+        print("test path url: ", path)
+        if url == '/': # THis if statement can probably be deleted later
+            return 200, path + 'index.html'
+        elif '/../' in path:
+            return 404, None
+        else:
+            if os.path.exists(path):
+                if os.path.isdir(path):
+                    if path.endswith('/'):
+                        return 200, path + 'index.html'
+                    else:
+                        return 303, url + '/'
+                else:
+                    return 200, path
+            else:
+                return 404, None
+        return
+    def send_http_headers(self, code, url):
         data = "HTTP/1.1 {} {}\r\n".format(code, http_code_dict[code])
         data += "Date: {}\r\n".format(formatdate(timeval=None, localtime=False, usegmt=True))
+        if url != None:
+            type = mimetypes.guess_type(url)
         if code >= 200 and code < 300:
             data += 'Content-Type: {}\r\n'.format(type[0])
+            message_body = self.get_data(url)
+            data += 'Content-Length: {}\r\n'.format(len(message_body.encode(FORMAT)))
             data += 'Connection: close\r\n\r\n'
-            data += str(msg)
+            data += str(message_body)
             return data
+        elif code == 303:
+            data += "Location: {}\r\n".format(url)
+            data += "Connection: close\r\n\r\n"
+            print(data)
+            return data
+
         elif code >=400 and code < 500:
             data += 'Connection: close\r\n\r\n'
             return data
         else:
             return
         
-
-    def do_GET(self, request):
-        if request == '/':
-
-            #with open('www/base.css', 'r') as f:
-                #data = f.read()
-
-            #TODO: FIX THE PATH AS IT WILL NOT WORK ON LAB MACHINES, ONLY ON THIS MACHINE!!!!!!!!!!!!
-            with open('www/index.html' ,'r') as f:
-                data = f.read()
-        
-            print(data)
-            return data
-
-        else:
-            path = CWD + request
-            print("PRINTING REQUEST: ", request)
-            #checks if the path is valid
-            if "/../" not in request:           # Lazy fix
-            #TODO: PROPERLY IMPLEMENT CHECKING IF THE REQUEST PATH IS IN /WWW/ DIR
-            #if Path(CWD) in Path(path).parents:
-                if os.path.exists(path):
-                    #TODO: CHECK IF THE PAHT IS A DIR
-                    with open(CWD+request, 'r') as f:
-                        data = f.read()
-                    return data
-                else:
-                    return
-            else:
-                print("PATH CHECK ELSE STATEMENT")
-                return
-        return
-
+    def get_data(self, path):
+        with open(path, 'r') as f:
+            data = f.read()
+        return data
 
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
-        '''
-        data = "HTTP/1.1 200 OK\r\n"
-        data += "TESTING \r\n"
-        data += "Connection: close \r\n\r\n"
-        '''
-        #self.request.sendall(bytearray("HTTP/1.1 200 OK",'utf-8'))
-        #self.request.sendall(bytearray("TESTING", FORMAT))
-        #self.request.sendall(bytearray(data, FORMAT))
-        print("UTF decode: ",self.data.decode(FORMAT))
-        #print("Attempting to print path")
-        #print(parse.urlparse(self.data.decode(FORMAT)).port)
         self.parse_request(self.data.decode(FORMAT))
 
 if __name__ == "__main__":
